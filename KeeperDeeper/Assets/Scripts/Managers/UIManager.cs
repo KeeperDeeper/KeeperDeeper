0d2ecde1cbc1a3ddb698a0fc7a8bb555deea81bb
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2024-07-30 03:37:50 +0900

    refactor: 프리팹 명명 및 경로 일관성 변경

--- KeeperDeeper/Assets/Scripts/Managers/UIManager.cs
+++ KeeperDeeper/Assets/Scripts/Managers/UIManager.cs
@@ -39,7 +39,7 @@ public class UIManager : IManagers
     {
         dynamicUICount++;
         uiCountsByType[uiType]++;
-        GameObject uiObj = GameObject.Instantiate(Resources.Load<GameObject>($"Prefabs/{uiType}"), mainCanvasTransform);
+        GameObject uiObj = UnityEngine.Object.Instantiate(Resources.Load<GameObject>($"Prefabs/UI/UI_{uiType}"), mainCanvasTransform);
         uiList.Add(uiObj.GetComponent<UI_Base>());
         uiList[dynamicUICount - 1].SetUIType(uiType);
     }

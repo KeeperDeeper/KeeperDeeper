9e269632165e6241b11b36d5698122ef63027650
LimHyeonGyun <200hk@naver.com>
2024-08-09 20:48:13 +0900

    feat:몬스터 충돌 작업 진행중

--- KeeperDeeper/Assets/Scripts/Monster/MonsterBehavior.cs
+++ KeeperDeeper/Assets/Scripts/Monster/MonsterBehavior.cs
@@ -2,51 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using OxygenSystem;
-using System.Runtime.CompilerServices;
 
-public class MonsterBehavior : MonoBehaviour
+namespace Monster
 {
-    [SerializeField]
-    private MonsterInformation monInfo;
-    private Oxygen oxygen;
-
-    private Vector2 spawnPoint;
-    private BoxCollider2D bodyollider;
-    private void Awake()
+    public class MonsterBehavior : MonoBehaviour
     {
-        bodyollider = GetComponent<BoxCollider2D>();
-        spawnPoint = this.transform.position;
-    }
+        public MonsterState monState;
 
-    void Start()
-    {
-        monInfo.InitMonster();
-    }
+        [SerializeField]
+        private MonsterInformation monInfo;
+        private Oxygen oxygen;
 
-    private void Update()
-    {
-        MoveMonster();
-    }
-    //몬스터 이동
-    private void MoveMonster()
-    {
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        private Vector2 spawnPoint;
+        private BoxCollider2D bodyollider;
+        private void Awake()
         {
-            oxygen = FindObjectOfType<Oxygen>();
-            CrashPlayer();
+            bodyollider = GetComponent<BoxCollider2D>();
+            spawnPoint = this.transform.position;
         }
-    }
 
-    public void CrashPlayer()
-    {
-        //1.플레이어 산소 감소
-        oxygen.DecreaseOxygen(monInfo.monsterInfo.attack);
-        //2.몬스터 삭제
-        Destroy(this.gameObject);
+        void Start()
+        {
+            monInfo.InitMonster();
+        }
+
+        private void Update()
+        {
+            if (monState == MonsterState.Move)
+            {
+                MoveMonster();
+            }
+            else if (monState == MonsterState.Chase)
+            {
+                ChaseMonster();
+            }
+        }
+        //몬스터 이동
+        private void MoveMonster()
+        {
+
+        }
+        private void ChaseMonster()
+        {
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                oxygen = FindObjectOfType<Oxygen>();
+                CrashPlayer();
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+
+        }
+
+        public void CrashPlayer()
+        {
+            //1.플레이어 산소 감소
+            oxygen.DecreaseOxygen(monInfo.monsterInfo.attack);
+            //2.충돌시 깜빡거림 효과
+            //3. 효과 이후 몬스터 삭제
+            Destroy(this.gameObject);
+        }
     }
 }

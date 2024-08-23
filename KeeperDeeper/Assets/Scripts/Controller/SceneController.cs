8bd95cbb3e6db2c4cce5c51018b87535af45b8db
LimHyeonGyun <200hk@naver.com>
2024-08-23 14:29:33 +0900

    feat: SceneController구현

--- KeeperDeeper/Assets/Scripts/Controller/SceneController.cs
+++ KeeperDeeper/Assets/Scripts/Controller/SceneController.cs
@@ -1,17 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            if (Input.GetKeyDown(KeyCode.F))
-            {
+    [SerializeField]
+    private Image fadeImage;
 
+    private void OnLevelWasLoaded(int level)
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void SceneChange(string sceneName)
+    {
+        StartCoroutine(FadeIn(sceneName));
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator FadeIn(string sceneName)
+    {
+        Color aColor = fadeImage.color;
+        aColor.a = 0;
+        fadeImage.color = aColor;
+
+        while (fadeImage.color.a > 1)
+        {
+            Color color = fadeImage.color;
+            color.a += Time.deltaTime;
+            fadeImage.color = color;
+            if (fadeImage.color.a > 1)
+            {
+                LoadScene(sceneName);
             }
+            yield return null;
+        }
+    }
+    IEnumerator FadeOut()
+    {
+        Color aColor = fadeImage.color;
+        aColor.a = 1;
+        fadeImage.color = aColor;
+
+        while (fadeImage.color.a <  0)
+        {
+            Color color = fadeImage.color;
+            color.a += Time.deltaTime;
+            fadeImage.color = color;
+            yield return null;
         }
     }
 }
